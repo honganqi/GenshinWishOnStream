20d57f7725236c7dde8998384f9266b2a5511e05
Archie Ang <82061978+honganqi@users.noreply.github.com>
2026-06-30 06:30:07 +0800

    add default profile settings

---
+++ browser_source/js/profiles/defaults/rates.js
@@ -0,0 +1,7 @@
+let rates = [];
+
+// To customize this, the syntax is "rates[x] = y"
+// where "x" is the star value and "y" is the pull rate (out of 100)
+rates[5] = 10;
+rates[4] = 25;
+rates[3] = 65;
